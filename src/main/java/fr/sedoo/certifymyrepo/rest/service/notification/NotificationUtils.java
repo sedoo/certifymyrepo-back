ac14603c457ac1e4271a9e352b459439fcefe697
Thomas Romuald <thomas.romuald@obs-mip.fr>
2021-08-05 16:51:27 +0200

    monthly reminder WIP

--- src/main/java/fr/sedoo/certifymyrepo/rest/service/notification/NotificationUtils.java
+++ src/main/java/fr/sedoo/certifymyrepo/rest/service/notification/NotificationUtils.java
@@ -1,18 +1,27 @@
 package fr.sedoo.certifymyrepo.rest.service.notification;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import fr.sedoo.certifymyrepo.rest.config.ApplicationConfig;
 import fr.sedoo.certifymyrepo.rest.dao.CertificationReportDao;
 import fr.sedoo.certifymyrepo.rest.dao.ProfileDao;
 import fr.sedoo.certifymyrepo.rest.dao.RepositoryDao;
 import fr.sedoo.certifymyrepo.rest.domain.CertificationReport;
+import fr.sedoo.certifymyrepo.rest.domain.Profile;
+import fr.sedoo.certifymyrepo.rest.domain.Repository;
+import fr.sedoo.certifymyrepo.rest.dto.ContactDto;
 
 @Component
 public class NotificationUtils {
@@ -31,6 +40,9 @@ public class NotificationUtils {
 	
 	@Autowired
 	EmailSender emailSender;
+	
+	@Autowired
+	private ApplicationConfig appConfig;
 
 	@Scheduled(cron = "${notification.cronExpression}")
 	public void notificationUnsedRepository() {
@@ -41,43 +53,49 @@ public class NotificationUtils {
 		List<CertificationReport> reportList = reportDao.findInProgressByUpdateDateLowerThan(c.getTime());
 		if(reportList != null) {
 			for(CertificationReport report : reportList) {
-				//buildNotification(report.getRepositoryId(), "", "");
+				buildNotification(report.getRepositoryId(), appConfig.getNoActivityNotificationSubject(), 
+						appConfig.getRepositoryAccessFrenchContent(),
+						appConfig.getNoActivityNotificationEnglishContent());
 			}
 		}
 	}
 	
-//	/**
-//	 * Build ContactDto object and send notification
-//	 * @param repositoryId repository identifier
-//	 * @param messages i18n bundle
-//	 * @param key i18n key prefix ("report.validation" or "report.new.version")
-//	 */
-//	private void buildNotification(String repositoryId, String key, String message) {
-//		// notification the report has been validated
-//		Repository repo = repositoryDao.findById(repositoryId);
-//		// List user id in DB
-//		List<String> repoUsersEmail = new ArrayList<String>();
-//		Set<String> repoUserIdList = repo.getUsers().stream().map(repoUser -> repoUser.getId()).collect(Collectors.toSet());
-//		for(String userId : repoUserIdList) {
-//			Optional<Profile> userProfile = profileDao.findById(userId);
-//			if(userProfile.isPresent() && userProfile.get().getEmail() != null) {
-//				repoUsersEmail.add(userProfile.get().getEmail());
-//			}
-//		}
-//		if( repoUsersEmail != null && repoUsersEmail.size() > 0) {
-//			ContactDto contact = new  ContactDto();
-//			Set<String> to = new HashSet<String>();
-//			to.addAll(repoUsersEmail);
-//			contact.setTo(to);
-//			contact.setSubject(String.format(messages.getString(key.concat(".notification.subject")), repo.getName()));
-//			if(message != null) {
-//				contact.setMessage(String.format(messages.getString(key.concat(".notification.content")), repo.getName(), message));	
-//			} else {
-//				contact.setMessage(String.format(messages.getString(key.concat(".notification.content")), repo.getName()));
-//			}
-//
-//			emailSender.sendNotification(contact);
-//		}
-//	}
+	/**
+	 * Build ContactDto object and send notification
+	 * @param repositoryId repository identifier
+	 * @param subject
+	 * @param frenchContent
+	 * @param englishContent
+	 * @param message optional
+	 */
+	private void buildNotification(String repositoryId, String subject, 
+			String frenchContent, String englishContent) {
+		// notification the report has been validated
+		Repository repo = repositoryDao.findById(repositoryId);
+		// List user id in DB
+		List<String> repoUsersEmail = new ArrayList<String>();
+		Set<String> repoUserIdList = repo.getUsers().stream().map(repoUser -> repoUser.getId()).collect(Collectors.toSet());
+		for(String userId : repoUserIdList) {
+			Optional<Profile> userProfile = profileDao.findById(userId);
+			if(userProfile.isPresent() && userProfile.get().getEmail() != null) {
+				repoUsersEmail.add(userProfile.get().getEmail());
+			}
+		}
+		if( repoUsersEmail != null && repoUsersEmail.size() > 0) {
+			ContactDto contact = new  ContactDto();
+			Set<String> to = new HashSet<String>();
+			to.addAll(repoUsersEmail);
+			contact.setTo(to);
+			contact.setSubject(String.format(subject, repo.getName(), repo.getName()));
+			String content = appConfig.getEnglishHeader().concat("<br/><br/>");
+			content = content.concat(String.format(frenchContent, repo.getName()))
+						.concat("<br/><br/>").concat(String.format(englishContent, repo.getName()));
+			contact.setMessage(content);
+
+			emailSender.sendNotification(contact);
+		}
+	}
+	
+
 
 }
