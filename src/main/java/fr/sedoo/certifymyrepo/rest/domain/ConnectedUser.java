50016b899909d6b6f11d11ab20025c2eb9c0ed25
Thomas Romuald <thomas.romuald@gmail.com>
2022-05-30 17:17:13 +0200

    fix connected user

--- src/main/java/fr/sedoo/certifymyrepo/rest/domain/ConnectedUser.java
+++ src/main/java/fr/sedoo/certifymyrepo/rest/domain/ConnectedUser.java
@@ -1,22 +1,32 @@
 package fr.sedoo.certifymyrepo.rest.domain;
 
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ConnectedUser {
+public class ConnectedUser implements Comparable<ConnectedUser>{
 	
-	public ConnectedUser(String userId, String fullName, String shortName, boolean isReadOnly) {
+	public ConnectedUser(String userId, String fullName, String shortName, boolean isReadOnly, Date creationDate) {
 		this.setUserId(userId);
 		this.setFullName(fullName);
 		this.setShortName(shortName);
 		this.setReadOnly(isReadOnly);
+		this.setCreationDate(creationDate);
 	}
 	
 	private String userId;
 	private String fullName;
 	private String shortName;
 	private boolean isReadOnly;
+	private Date creationDate;
+	
+	@Override
+	public int compareTo(ConnectedUser arg0) {
+		// TODO Auto-generated method stub
+		return this.getCreationDate().compareTo(arg0.getCreationDate());
+	}
 
 }
