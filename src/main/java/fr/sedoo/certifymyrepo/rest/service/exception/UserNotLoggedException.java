53b7d843143fd9557f0ea5a816af19c61757de9a
Thomas ROMUALD <thomas.romuald@obs-mip.fr>
2024-01-16 14:23:57 +0100

    add HttpStatus.FORBIDDEN to UserNotLoggedException

--- src/main/java/fr/sedoo/certifymyrepo/rest/service/exception/UserNotLoggedException.java
+++ src/main/java/fr/sedoo/certifymyrepo/rest/service/exception/UserNotLoggedException.java
@@ -1,5 +1,9 @@
 package fr.sedoo.certifymyrepo.rest.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UserNotLoggedException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 }
